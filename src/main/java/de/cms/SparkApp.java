dc511ba8753b565a86c99a3681cce21971c4b40d
Timor <timor@cyhex.com>
2015-09-03 16:46:06 +0200

    Initial commit

---
+++ src/main/java/de/cms/SparkApp.java
@@ -0,0 +1,24 @@
+package de.cms;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SparkApp {
+
+    private static final Logger log = LogManager.getLogger(SparkApp.class);
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("Log Counter");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> logData = sc.textFile("/home/gx/Documents/projects/spark-test/data/access.log");
+
+        long count = logData.filter(s -> s.contains("\"GET / HTTP/1.1\" 200")).count();
+
+        log.info("Total responces: " + logData.count());
+        log.info("200 responces: " + count);
+    }
+}
---
+++ src/main/java/de/cms/StreamApp.java
@@ -0,0 +1,31 @@
+package de.cms;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+
+public class StreamApp {
+
+    private static final Logger log = LogManager.getLogger(StreamApp.class);
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("Network World Counter");
+        JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(1));
+        JavaReceiverInputDStream<String> lines = sc.socketTextStream("localhost", 9999);
+        JavaDStream<String> stringJavaDStream = lines.flatMap(o -> Arrays.asList(o.split(" ")));
+        stringJavaDStream
+                .mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i, i2) -> i + i2).print();
+        sc.start();
+        sc.awaitTermination();
+
+    }
+}
